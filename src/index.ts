c6e456feabd759e0b70e5edbd72476ddb6cfbe3d
Torleif Berger <torleif@outlook.com>
2020-01-31 01:11:23 +0100

    Removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 /// <reference path="./types.ts"/>
 
 import navigate from './navigate';
-import { HISTORY } from './common';
-import { createBrowserHistory as createHistory } from 'history';
 
 Cypress.Commands.add('navigate', { prevSubject: false }, navigate);
